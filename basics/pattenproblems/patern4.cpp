51c6027c3e0a56633f6b70b06c63d5435f43f865
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-06-26 07:59:26 +0530

    solved the pyramid patten question

--- basics/pattenproblems/patern4.cpp
+++ basics/pattenproblems/patern4.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
 using namespace std;
+// int main(){
+   
+    
+//         int n;
+//         cin>>n;
+//         int i = 1;
+//         while(i<=n)
+//         {
+//             int j = 0;
+//             while(j<i)
+//             {
+//              cout<<i+j<<" ";
+//              j=j+1;   
+//             }
+//             cout<<endl;
+//             i=i+1;
+//         }
+        
+    
+// }
+
+
 int main(){
    
     
@@ -11,7 +33,7 @@ int main(){
             int j = 0;
             while(j<i)
             {
-             cout<<i+j<<" ";
+             cout<<i-j<<" ";
              j=j+1;   
             }
             cout<<endl;
